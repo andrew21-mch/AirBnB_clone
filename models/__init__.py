f71a8173250d0411be58cc963d8b8b29d75ce77a
andrew21-mch <nfonandrew73@gmail.com>
2021-11-22 14:11:24 +0100

    basic page

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,7 @@
 from . import base_model
-from . import user
 from .engine.file_storage import FileStorage
 
-all = [base_model.BaseModel, user.User]
+all = [base_model.BaseModel]
 
 storage = FileStorage()
 storage.reload()
