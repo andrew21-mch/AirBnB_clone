c83b584f759a70747cacc070b4b471b18e9e0857
andrew21-mch <nfonandrew73@gmail.com>
2021-11-22 17:47:18 +0100

    Airbnb models

--- models/base_model.py
+++ models/base_model.py
@@ -37,34 +37,35 @@ class BaseModel:
     created_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
     updated_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
 
+    def __init__(self, *args, **kwargs):
+        """constructor"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+            models.storage.new(self)
 
     def __str__(self):
         """ String representation"""
-        return f'{self.__class__.__name__} {self.id}  {self.__dict__}'
+        return self.__class__.__name__, self.id,\
+        self.__dict__.__format__("{:s} {:s} {}")
 
     def save(self):
-        """
-        updates the updated_at public instance variable
-        """
-
-        self.updated_at = datetime.today()
+        self.updated_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
         models.storage.save()
 
-
-
     def to_dict(self):
-        def to_dict(self):
-            """
-                return the dictionary representation of the object to_dict
-            """
-
-        obj_repr = {}
-        for key, value in self.__dict__.items():
-            if key == "created_at" or key == "updated_at":
-                obj_repr[key] = value.isoformat()
-            else:
-                obj_repr[key] = value
-
-        obj_repr["__class__"] = self.__class__.__name__
-
-        return obj_repr
+        """ returns a dictionary containing all keys/values
+        of __dict__ of the instance"""
+        to_dict = dict(self.__dict__)
+        to_dict['__class__'] = self.__class__.__name__
+        to_dict['updated_at'] = self.updated_at\
+        .strftime("%Y-%m-%dT%H:%M:%S.%f")
+        to_dict['created_at'] = self.created_at\
+        .strftime("%Y-%m-%dT%H:%M:%S.%f")
+        return to_dict
