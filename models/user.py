cc31adc85cbca6413f91552af1cc2672b5f2ad2b
andrew21-mch <nfonandrew73@gmail.com>
2022-08-07 16:20:04 +0100

    user model

--- models/user.py
+++ models/user.py
@@ -17,5 +17,16 @@ class User(BaseModel):
     password = ''
     first_name = ''
     last_name = ''
-    pass
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__class__ = User
+        self.email = kwargs.get('email', '')
+        self.password = kwargs.get('password', '')
+        self.first_name = kwargs.get('first_name', '')
+        self.last_name = kwargs.get('last_name', '')
+
+    def __str__(self):
+        return "[User] ({}) {}".format(self.id, self.__dict__)
+        
 
