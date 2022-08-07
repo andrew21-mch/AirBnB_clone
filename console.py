034aae145a20e166de9cc120c79335b0d89e31cc
andrew21-mch <nfonandrew73@gmail.com>
2022-08-07 15:21:31 +0100

    some deletions

--- console.py
+++ console.py
@@ -41,7 +41,7 @@ class HBNBCommand(cmd.Cmd):
         """
         return True
 
-    def do_EOF(self, line):
+    def EOF(self, line):
         """
         quit console on EOF
         """
@@ -129,7 +129,7 @@ class HBNBCommand(cmd.Cmd):
         else:
             print('** ** Accepted values are 1, 2 and 4 **')
 
-    def do_create(self, line):
+    def create(self, line):
         """
         Creates an instance from the valid model argument
         """
@@ -140,7 +140,7 @@ class HBNBCommand(cmd.Cmd):
             models.storage.new(obj)
             obj.save()
 
-    def do_show(self, line: str):
+    def show(self, line: str):
         """
         Show instance of model with specified id
         """
@@ -156,7 +156,7 @@ class HBNBCommand(cmd.Cmd):
     #     # if instance:
     #     #     print(f"** {instance.id} destroyed **")
 
-    def do_all(self, line):
+    def all(self, line):
         """
         Prints all string representation of all instances
         based or not on the class name
@@ -177,7 +177,7 @@ class HBNBCommand(cmd.Cmd):
         if array:
             print(array)
 
-    def do_update(self, line):
+    def update(self, line):
         """
          Updates an instance based on the class name and id
          by adding or updating attribute
